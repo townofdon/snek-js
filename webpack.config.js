b9f197bf771a694794a636ae4daa2b3515e837ce
Don Juan Javier <townofdon@gmail.com>
2023-10-18 02:45:25 -0400

    remove unnecessary webpack json loader

--- webpack.config.js
+++ webpack.config.js
@@ -40,12 +40,6 @@ const config = {
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-    loaders: [
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      }
-    ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
