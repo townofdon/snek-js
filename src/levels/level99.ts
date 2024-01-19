5e66fd52036d04d6c97bef20f2f945cc4cacf034
Don Juan Javier <townofdon@gmail.com>
2024-01-19 03:38:55 -0500

    make level99 interesting

--- src/levels/level99.ts
+++ src/levels/level99.ts
@@ -9,38 +9,38 @@ export const LEVEL_99: Level = {
   timeToClear: 1000 * 60 * 10,
   applesToClear: 200,
   numApplesStart: 20,
-  growthMod: 0.9,
+  growthMod: 0.7,
   layout: `
-XXXXXX44XXXXXXddXXXXXX44XXXXXX
+XXXXXX44XXXXXXLLXXXXXX44XXXXXX
 X~~~~~__~~~~XXddXX~~~~__~~~~~X
-X~~   --  ~~~~__~~~~  --   ~~X
+X~~   --  ~~~~__~~~~  --   k~X
 X~    --    ~~__~~    --    ~X
-X~    --      ==      --    ~X
-X~    --      ==      --    ~X
+X~    --      KK      --    ~X
+X~    --      KK      --    ~X
 X~    --      --      --    ~X
-0_----==      --      ==----_1
-0_----==      --      ==----_1
+0_----LL      --      LL----_1
+0_----LL      --      LL----_1
 X~            --            ~X
 X~            --            ~X
 X~~           --           ~~X
-XX~           ==           ~XX
-XX~~        --==--        ~~XX
-dd__==------==  ==------==__dd
-dd__==------==  ==------==__dd
-XX~~        --==--        ~~XX
-XX~           ==           ~XX
+XX~           KK           ~XX
+XX~~        -_dd_-        ~~XX
+Ld__KK------KdladK------KK__dL
+Ld__KK------KdaadK------KK__dL
+XX~~        -_dd_-        ~~XX
+XX~           KK           ~XX
 X~~           --           ~~X
+X~        o   --            ~X
 X~            --            ~X
-X~            --            ~X
-0_----==      --      ==----_1
-0_----==      --      ==----_1
+0_----LL      --      LL----_1
+0_----LL      --      LL----_1
 X~    --      --      --    ~X
-X~    --      ==      --    ~X
-X~    --      ==      --    ~X
+X~    --      KK      --    ~X
+X~    --      KK      --    ~X
 X~    --    ~~__~~    --    ~X
 X~~   --  ~~~~__~~~~  --   ~~X
 X~~~~~__~~~~XXddXX~~~~__~~~~~X
-XXXXXX33XXXXXXddXXXXXX33XXXXXX
+XXXXXX33XXXXXXLLXXXXXX33XXXXXX
   `,
   colors: PALETTE.darkStar,
   titleScene: (p5, sfx, fonts, callbacks) => new TitleScene(name, p5, sfx, fonts, callbacks),
