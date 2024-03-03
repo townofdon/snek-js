d6c19b5cee3a8b37358a0092a7b44730df988959
Don Juan Javier <townofdon@gmail.com>
2024-03-03 14:09:15 -0500

    style pause menu buttons

--- src/snek.ts
+++ src/snek.ts
@@ -2345,8 +2345,8 @@ export const sketch = (p5: P5) => {
     UI.drawDarkOverlay(uiElements);
     UI.drawText('PAUSED', '30px', 235, uiElements, { color: ACCENT_COLOR });
     UI.drawText('[ESC] To Unpause', '14px', 285, uiElements);
-    UI.drawButton("MAIN MENU", 20, 20, confirmShowMainMenu, uiElements).addClass('minimood').id('pauseButtonMainMenu');
-    UI.drawButton("SETTINGS", 445, 20, showInGameSettingsMenu, uiElements).addClass('minimood').id('pauseButtonSettings');
+    UI.drawButton("MAIN MENU", 20, 20, confirmShowMainMenu, uiElements).addClass('minimood').addClass('focus-invert').id('pauseButtonMainMenu');
+    UI.drawButton("SETTINGS", 445, 20, showInGameSettingsMenu, uiElements).addClass('minimood').addClass('focus-invert').id('pauseButtonSettings');
 
     if (!queryParams.enableWarp && !state.isCasualModeEnabled || level === START_LEVEL) {
       return;
@@ -2359,33 +2359,33 @@ export const sketch = (p5: P5) => {
     const yRow3 = 520;
     const yRow4 = 560;
     let x = xInitial;
-    UI.drawButton("01", x + 0.00000, yRow1, () => warpToLevel(1), uiElements).id('pauseButtonWarp01');
-    UI.drawButton("02", x += offset, yRow1, () => warpToLevel(2), uiElements).id('pauseButtonWarp02');
-    UI.drawButton("03", x += offset, yRow1, () => warpToLevel(3), uiElements).id('pauseButtonWarp03');
-    UI.drawButton("04", x += offset, yRow1, () => warpToLevel(4), uiElements).id('pauseButtonWarp04');
-    UI.drawButton("05", x += offset, yRow1, () => warpToLevel(5), uiElements).id('pauseButtonWarp05');
-    UI.drawButton("06", x += offset, yRow1, () => warpToLevel(6), uiElements).id('pauseButtonWarp06');
+    UI.drawButton("01", x + 0.00000, yRow1, () => warpToLevel(1), uiElements).addClass('focus-invert').id('pauseButtonWarp01');
+    UI.drawButton("02", x += offset, yRow1, () => warpToLevel(2), uiElements).addClass('focus-invert').id('pauseButtonWarp02');
+    UI.drawButton("03", x += offset, yRow1, () => warpToLevel(3), uiElements).addClass('focus-invert').id('pauseButtonWarp03');
+    UI.drawButton("04", x += offset, yRow1, () => warpToLevel(4), uiElements).addClass('focus-invert').id('pauseButtonWarp04');
+    UI.drawButton("05", x += offset, yRow1, () => warpToLevel(5), uiElements).addClass('focus-invert').id('pauseButtonWarp05');
+    UI.drawButton("06", x += offset, yRow1, () => warpToLevel(6), uiElements).addClass('focus-invert').id('pauseButtonWarp06');
     x = xInitial;
-    UI.drawButton("07", x + 0.00000, yRow2, () => warpToLevel(7), uiElements).id('pauseButtonWarp07');
-    UI.drawButton("08", x += offset, yRow2, () => warpToLevel(8), uiElements).id('pauseButtonWarp08');
-    UI.drawButton("09", x += offset, yRow2, () => warpToLevel(9), uiElements).id('pauseButtonWarp09');
-    UI.drawButton("10", x += offset, yRow2, () => warpToLevel(10), uiElements).id('pauseButtonWarp10');
-    UI.drawButton("11", x += offset, yRow2, () => warpToLevel(11), uiElements).id('pauseButtonWarp11');
-    UI.drawButton("12", x += offset, yRow2, () => warpToLevel(12), uiElements).id('pauseButtonWarp12');
+    UI.drawButton("07", x + 0.00000, yRow2, () => warpToLevel(7), uiElements).addClass('focus-invert').id('pauseButtonWarp07');
+    UI.drawButton("08", x += offset, yRow2, () => warpToLevel(8), uiElements).addClass('focus-invert').id('pauseButtonWarp08');
+    UI.drawButton("09", x += offset, yRow2, () => warpToLevel(9), uiElements).addClass('focus-invert').id('pauseButtonWarp09');
+    UI.drawButton("10", x += offset, yRow2, () => warpToLevel(10), uiElements).addClass('focus-invert').id('pauseButtonWarp10');
+    UI.drawButton("11", x += offset, yRow2, () => warpToLevel(11), uiElements).addClass('focus-invert').id('pauseButtonWarp11');
+    UI.drawButton("12", x += offset, yRow2, () => warpToLevel(12), uiElements).addClass('focus-invert').id('pauseButtonWarp12');
     x = xInitial;
-    UI.drawButton("13", x + 0.00000, yRow3, () => warpToLevel(13), uiElements).id('pauseButtonWarp13');
-    UI.drawButton("14", x += offset, yRow3, () => warpToLevel(14), uiElements).id('pauseButtonWarp14');
-    UI.drawButton("15", x += offset, yRow3, () => warpToLevel(15), uiElements).id('pauseButtonWarp15');
-    UI.drawButton("16", x += offset, yRow3, () => warpToLevel(16), uiElements).id('pauseButtonWarp16');
-    UI.drawButton("17", x += offset, yRow3, () => warpToLevel(17), uiElements).id('pauseButtonWarp17');
-    UI.drawButton("18", x += offset, yRow3, () => warpToLevel(18), uiElements).id('pauseButtonWarp18');
+    UI.drawButton("13", x + 0.00000, yRow3, () => warpToLevel(13), uiElements).addClass('focus-invert').id('pauseButtonWarp13');
+    UI.drawButton("14", x += offset, yRow3, () => warpToLevel(14), uiElements).addClass('focus-invert').id('pauseButtonWarp14');
+    UI.drawButton("15", x += offset, yRow3, () => warpToLevel(15), uiElements).addClass('focus-invert').id('pauseButtonWarp15');
+    UI.drawButton("16", x += offset, yRow3, () => warpToLevel(16), uiElements).addClass('focus-invert').id('pauseButtonWarp16');
+    UI.drawButton("17", x += offset, yRow3, () => warpToLevel(17), uiElements).addClass('focus-invert').id('pauseButtonWarp17');
+    UI.drawButton("18", x += offset, yRow3, () => warpToLevel(18), uiElements).addClass('focus-invert').id('pauseButtonWarp18');
     x = xInitial;
-    UI.drawButton("19", x + 0.00000, yRow4, () => warpToLevel(19), uiElements).id('pauseButtonWarp19');
-    UI.drawButton("20", x += offset, yRow4, () => warpToLevel(99), uiElements).id('pauseButtonWarp20');
-    UI.drawButton("S1", x += offset, yRow4, () => warpToLevel(110), uiElements).id('pauseButtonWarpS1');
-    UI.drawButton("S2", x += offset, yRow4, () => warpToLevel(120), uiElements).id('pauseButtonWarpS2');
-    UI.drawButton("S3", x += offset, yRow4, () => warpToLevel(130), uiElements).id('pauseButtonWarpS3');
-    UI.drawButton("S4", x += offset, yRow4, () => warpToLevel(140), uiElements).id('pauseButtonWarpS4');
+    UI.drawButton("19", x + 0.00000, yRow4, () => warpToLevel(19), uiElements).addClass('focus-invert').id('pauseButtonWarp19');
+    UI.drawButton("20", x += offset, yRow4, () => warpToLevel(99), uiElements).addClass('focus-invert').id('pauseButtonWarp20');
+    UI.drawButton("S1", x += offset, yRow4, () => warpToLevel(110), uiElements).addClass('focus-invert').id('pauseButtonWarpS1');
+    UI.drawButton("S2", x += offset, yRow4, () => warpToLevel(120), uiElements).addClass('focus-invert').id('pauseButtonWarpS2');
+    UI.drawButton("S3", x += offset, yRow4, () => warpToLevel(130), uiElements).addClass('focus-invert').id('pauseButtonWarpS3');
+    UI.drawButton("S4", x += offset, yRow4, () => warpToLevel(140), uiElements).addClass('focus-invert').id('pauseButtonWarpS4');
   }
 
   function openDoors() {
