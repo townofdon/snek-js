bc4f14a2a8737d53f1306a15260749859fd37e51
Don Juan Javier <townofdon@gmail.com>
2024-06-17 05:51:02 -0400

    fix import casing

--- src/community/index.tsx
+++ src/community/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import { App } from "./app";
+import { App } from "./App";
 
 const app = document.getElementById('app')
 if (!app) throw new Error('no element found for id="app"')
